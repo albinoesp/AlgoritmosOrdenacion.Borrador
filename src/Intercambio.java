ddb142962cc3c5a479f95fb37927d3ca07ed2477
AlbinoJ <jesusdavida.e2089@gmail.com>
2022-09-30 12:47:45 -0700

    Algoritmos de Ordenación: Borrador
    Actualización 2
    
    - Solo se realizaron comentatrios para Intercambio.java y SelectionSort.java

--- src/Intercambio.java
+++ src/Intercambio.java
@@ -3,12 +3,12 @@ public class Intercambio {
         int aux; //apoyo para hacer los intercambios
         System.out.println("");
         System.out.println("-----Intercambio-----");
-        for (int i = 0; i < array.length; i++){
-            for (int j = i + 1; j < array.length; j++){
-                if(array[i] > array[j]){
-                    aux = array[i];
-                    array[i] = array[j];
-                    array[j] = aux;
+        for (int i = 0; i < array.length; i++){ //Elemento inical
+            for (int j = i + 1; j < array.length; j++){ //Recorre el arreglo
+                if(array[i] > array[j]){ //Cambiar solo si el elemento en i resulta mayor a j (Ordenamiento ascendente)
+                    aux = array[i]; //Guardamos el elemento en una variable de apoyo
+                    array[i] = array[j]; //Cambiamos los elementos
+                    array[j] = aux; //Guardamos el elemento i (que estaba en la vaariable de apoyo) con la posición j
                 }
             }
         }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -5,18 +5,18 @@ public class SelectionSort {
         int aux; //auxiliar para el intercambio de valores
         System.out.println("");
         System.out.println("-----SelectionSort-----");
-        for (int i = 0; i < array.length; i++){
-            men = array[i];
-            ind = i;
-            for (int j = i + 1; j < array.length; j++){
-                if (array[j] < men){
-                    men = array[j];
-                    ind = j;
+        for (int i = 0; i < array.length; i++){ //Elemento i al que cambiará el valor
+            men = array[i]; //No sabemos cuál es el menor, declaramos al primero como el menor
+            ind = i; // Guardamos el indice del menor en el primer elemento
+            for (int j = i + 1; j < array.length; j++){ // Recorrer el arreglo
+                if (array[j] < men){ //Para cada elemento comparamos con el menor
+                    men = array[j]; //guardar el elemento menor
+                    ind = j; // Si el elemento resulta menor guardamos su posición
                 }
             }
-            aux = array[i];
-            array[i] = men;
-            array[ind] = aux;
+            aux = array[i]; //auxiliar para realizar el intercambio
+            array[i] = men; //Cambiamos i por el menor (orden ascendente)
+            array[ind] = aux; //intercambiamos el valor anterior de i con el valor menor encontrado
         }
         ordenacionMain.ArrayImp(array); //Imprimir array
         System.out.println("-----SelectionSort Fin-----");
